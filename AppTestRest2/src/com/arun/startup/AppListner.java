d7aa9e2e194f39e3e039a32a574f692927421d37
Arun Kunhikrishnan (Arun_K) <arun.kunhikrishnan@alacriti.com>
2019-03-08 18:05:47 +0530

    added application loading class which will be involked on application deployment

---
+++ AppTestRest2/src/com/arun/startup/AppListner.java
@@ -0,0 +1,23 @@
+package com.arun.startup;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class AppListner implements ServletContextListener{
+
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("contextDestroyed(ServletContextEvent arg0) ########################");
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("contextInitialized(ServletContextEvent arg0)$$$$$$$$$$$$$$$$$$$$$$$$$$");
+	}
+}
